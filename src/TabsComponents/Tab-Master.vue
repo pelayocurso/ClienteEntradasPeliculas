f5bb9774636ee806361b7112abc3d6a74c37c99c
pelayocurso <pelayocurso@gmail.com>
2017-08-25 18:40:12 +0200

    tabs recien empezados

---
+++ src/TabsComponents/Tab-Master.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul class="nav nav-tabs">
+    <tab></tab>
+  </ul>
+</template>
+
+<script>
+  import Tab from './Tab.vue';
+
+  export default {
+    name: 'tab-master',
+    components: Tab,
+  };
+</script>
---
+++ src/TabsComponents/Tab.vue
@@ -0,0 +1,12 @@
+<template>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#">Disabled</a>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'tab',
+
+  }
+</script>
