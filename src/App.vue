cbfe1f360578b085dec8798c21abc4a0e440bdc0
pelayocurso <pelayocurso@gmail.com>
2017-08-25 18:26:53 +0200

    Creado componente 'Modal'

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <modal></modal>
+
+    <tab-master></tab-master>
+  </div>
+</template>
+
+<script>
+import Modal from './Modal.vue';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :class="{'modal', 'fade', 'show': show}">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{{ title }}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="closeButton">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>{{ body }}</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" data-dismiss="modal" @click="closeButton">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'modal',
+
+    data() {
+      title: '',
+      body: '',
+      show: false
+    },
+
+    mounted() {
+      let _this = this;
+      Vue.$on('show', (title, body) => {
+        _this.title = title;
+        _this.body = body;
+        _this.show = true;
+      });
+    },
+
+    methods: {
+      closeButton() {
+        _this.show = false;
+      }
+    }
+  };
+
+</script>
+
+<style>
+</style>
